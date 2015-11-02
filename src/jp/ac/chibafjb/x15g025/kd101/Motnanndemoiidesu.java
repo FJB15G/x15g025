d311c97ecd43790061a7efb9c8ef5a216df7dca7
unknown <x15g025@PC504N027.chiba-fjb.ac.jp>
2015-11-02 14:40:03 +0900

    ﾋｬｸﾆ!

---
+++ src/jp/ac/chibafjb/x15g025/kd101/Motnanndemoiidesu.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g025.kd101;
+
+public class Motnanndemoiidesu {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=1;i<11;i++){
+		System.out.println( i + "船橋 太郎（改行）");
+	}
+	}
+}
