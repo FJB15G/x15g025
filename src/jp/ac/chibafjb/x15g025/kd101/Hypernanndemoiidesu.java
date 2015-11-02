15c3438aefb80f7bde0d6b5793d94e0d0f0545a8
unknown <x15g025@PC504N027.chiba-fjb.ac.jp>
2015-11-02 14:51:40 +0900

    いろいろ

---
+++ src/jp/ac/chibafjb/x15g025/kd101/Hypernanndemoiidesu.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g025.kd101;
+
+public class Hypernanndemoiidesu {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("氏名：　谷川　カイト（たにかわ　かいと）（改行）");
+		System.out.println("てててててててて");
+		System.out.println("ゴウランガ！");
+	}
+
+}
--- src/jp/ac/chibafjb/x15g025/kd101/Tyounanndemoiidesu.java
+++ src/jp/ac/chibafjb/x15g025/kd101/Tyounanndemoiidesu.java
@@ -5,8 +5,8 @@ public class Tyounanndemoiidesu {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		int i;
-		for(i=1;i<11;i++){
-		System.out.print( i + " 船橋 太郎 ");
+		for(i=0;i<3;i++){
+		System.out.println("あいうえお あいうえお あいうえお(改行)");
 	}
 	}
 
