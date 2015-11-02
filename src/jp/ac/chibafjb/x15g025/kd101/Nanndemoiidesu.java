8841ea1f41b3d71ad49dde03dcb72a656654566f
unknown <x15g025@PC504N027.chiba-fjb.ac.jp>
2015-11-02 14:25:13 +0900

    初コミット

---
+++ src/jp/ac/chibafjb/x15g025/kd101/Nanndemoiidesu.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g025.kd101;
+
+public class Nanndemoiidesu {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("船橋情報ビジネス専門学校（改行）");
+		System.out.println("047-425-1051（改行）");
+		System.out.print("Hello☆こんにちは（改行）");
+	}
+
+}
