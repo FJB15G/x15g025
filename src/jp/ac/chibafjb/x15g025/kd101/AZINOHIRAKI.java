a880596119953dc948c5711053d9fa16056cf72e
unknown <x15g025@PC504N027.chiba-fjb.ac.jp>
2015-11-02 14:56:12 +0900

    AZINOHIRAKI

---
+++ src/jp/ac/chibafjb/x15g025/kd101/AZINOHIRAKI.java
@@ -0,0 +1,17 @@
+package jp.ac.chibafjb.x15g025.kd101;
+
+public class AZINOHIRAKI {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("   a@@@a a@@@a");
+		System.out.println("  @@@@@@e@@@@@@");
+		System.out.println(" (@@@@@@@@@@@@@)");
+		System.out.println("  @@@@@@@@@@@@@");
+		System.out.println("   @@@@@@@@@@@");
+		System.out.println("    ~@@@@@@@~");
+		System.out.println("      ~@@@~");
+		System.out.println("       ~@~      ハート！");
+	}
+
+}
