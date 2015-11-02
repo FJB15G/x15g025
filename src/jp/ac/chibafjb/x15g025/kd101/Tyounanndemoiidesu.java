3ed5e19670e48e34735387fc0901f5b7d5acfb45
unknown <x15g025@PC504N027.chiba-fjb.ac.jp>
2015-11-02 14:44:34 +0900

    103

---
+++ src/jp/ac/chibafjb/x15g025/kd101/Tyounanndemoiidesu.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g025.kd101;
+
+public class Tyounanndemoiidesu {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=1;i<11;i++){
+		System.out.print( i + " 船橋 太郎 ");
+	}
+	}
+
+}
