9b2208cf1854bb01a3eb65c24be9ee2a86069e2f
unknown <x15g025@PC504N027.chiba-fjb.ac.jp>
2015-11-02 15:10:22 +0900

    ５レツ

---
+++ src/jp/ac/chibafjb/x15g025/kd101/Supernanndemoiidesu.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g025.kd101;
+
+public class Supernanndemoiidesu {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=0;i<5;i++){
+			System.out.println("＊＊＊（改行）");
+		}
+	}
+
+}
