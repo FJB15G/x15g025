5e2ad2e3fd23c51231d0ae3574d9b8532a64faa7
unknown <x15g025@PC504N027.chiba-fjb.ac.jp>
2015-11-09 14:20:57 +0900

    no message

---
+++ src/jp/ac/chibafjb/x15g025/kdT02/T02.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g025.kdT02;
+
+public class T02 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=0;i<3;i++){
+			System.out.println(i+100);
+		}
+	}
+
+}
