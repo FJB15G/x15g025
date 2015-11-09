5e7776f815dd0f9104fe546123f3fad07718ec71
unknown <x15g025@PC504N027.chiba-fjb.ac.jp>
2015-11-09 14:43:20 +0900

    no message

---
+++ src/jp/ac/chibafjb/x15g025/kdT141/C141.java
@@ -0,0 +1,16 @@
+package jp.ac.chibafjb.x15g025.kdT141;
+
+import java.util.Scanner;
+
+public class C141 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner sin = new Scanner(System.in);
+		System.out.println("整数を１つ入力してください。");
+		int i = sin.nextInt();    //整数値の入力
+		sin.close();
+		System.out.print("あなたの入力した数は"+i+"です。");
+	}
+
+}
