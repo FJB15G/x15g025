1358ca18e43b58f78f7a255f4b6ca7cad014febe
unknown <x15g025@PC504N027.chiba-fjb.ac.jp>
2015-11-09 14:07:09 +0900

    no message

---
+++ src/jp/ac/chibafjb/x15g025/kd111/C111.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g025.kd111;
+
+public class C111 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=0;i<10;i++){
+			System.out.println("船橋太郎");
+		}
+	}
+
+}
---
+++ src/jp/ac/chibafjb/x15g025/kd112/C112.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g025.kd112;
+
+public class C112 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=0;i<100;i++){
+			System.out.println("船橋情報ビジネス専門学校");
+		}
+	}
+
+}
