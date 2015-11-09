358b6d012de80007e49f25139021818dc5f76d30
unknown <x15g025@PC504N027.chiba-fjb.ac.jp>
2015-11-09 15:02:06 +0900

    no message

---
+++ src/jp/ac/chibafjb/x15g025/kdT144/C144.java
@@ -0,0 +1,23 @@
+package jp.ac.chibafjb.x15g025.kdT144;
+
+import java.util.Scanner;
+
+public class C144 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner sin = new Scanner(System.in);
+		System.out.println("住所の入力");		
+		String a = sin.next();    //整数値の入力
+		System.out.println("氏名の入力");
+		String b = sin.next(); 
+		System.out.println("電話番号の入力");
+		String c = sin.next(); 
+		System.out.println("<確認>住所："+a);
+		System.out.println("氏名："+b);
+		System.out.println("電話番号："+c);
+
+		sin.close();
+	}
+
+}
