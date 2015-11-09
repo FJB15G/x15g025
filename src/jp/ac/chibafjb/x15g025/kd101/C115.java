bcf54b8452b91c55c5e43776864b1dee45df615c
unknown <x15g025@PC504N027.chiba-fjb.ac.jp>
2015-11-09 13:54:51 +0900

    115116

---
+++ src/jp/ac/chibafjb/x15g025/kd101/C115.java
@@ -0,0 +1,19 @@
+package jp.ac.chibafjb.x15g025.kd101;
+
+public class C115 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+int i;
+for(i=0;i<2;i++){
+	System.out.println("あいうえお あいうえお あいうえお");
+}
+for(i=0;i<2;i++){
+	System.out.println("かきくけこ かきくけこ かきくけこ");
+	}
+for(i=0;i<2;i++){
+	System.out.println("さしすせそ さしすせそ さしすせそ");
+	}
+}
+	}
+
---
+++ src/jp/ac/chibafjb/x15g025/kd101/C116.java
@@ -0,0 +1,16 @@
+package jp.ac.chibafjb.x15g025.kd101;
+
+public class C116 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=0;i<2;i++){
+			System.out.println("あいうえお あいうえお あいうえお");
+			System.out.println("かきくけこ かきくけこ かきくけこ");
+			System.out.println("さしすせそ さしすせそ さしすせそ");
+		}
+
+
+	}
+}
