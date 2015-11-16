6f2622e83fe2340394113d90f08c4012f29c0c2f
unknown <x15g025@PC504N027.chiba-fjb.ac.jp>
2015-11-16 14:57:28 +0900

    164まだ

---
+++ src/jp/ac/chibafjb/x15g025/kd163/C163.java
@@ -0,0 +1,19 @@
+package jp.ac.chibafjb.x15g025.kd163;
+
+public class C163 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x[] = {33,18,102,47,5};
+		int sum;
+		sum = 0;
+		int i;
+		int n;
+		n = 5;
+		for(i=0;i<n;i++){
+			sum = sum + x[i];
+		}
+		System.out.println("合計 = "+sum);
+	}
+
+}
---
+++ src/jp/ac/chibafjb/x15g025/kd164/C164.java
@@ -0,0 +1,10 @@
+package jp.ac.chibafjb.x15g025.kd164;
+
+public class C164 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
