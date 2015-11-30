0f233ada1928270e2882522a05b13b25bba24668
unknown <x15g025@PC504N027.chiba-fjb.ac.jp>
2015-11-30 15:11:01 +0900

    C173

--- src/jp/ac/chibafjb/x15g025/kd165/C165.java
+++ src/jp/ac/chibafjb/x15g025/kd165/C165.java
@@ -4,18 +4,16 @@ public class C165 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		int a[] = {5,10,15};
+		int a[] = { 5, 10, 15 };
 		int b[] = new int[3];
-		
+
 		int m;
 		m = 3;
-		for(int i=0;i<m;i++){
-			b[i] = a[m-i-1];
-			System.out.println("b["+i+"] = "+b[i]);
+		for (int i = 0; i < m; i++) {
+			b[i] = a[m - i - 1];
+			System.out.println("b[" + i + "] = " + b[i]);
 		}
-		
-		
+
 	}
 
 }
-
--- src/jp/ac/chibafjb/x15g025/kd173/C173.java
+++ src/jp/ac/chibafjb/x15g025/kd173/C173.java
@@ -1,10 +1,43 @@
 package jp.ac.chibafjb.x15g025.kd173;
 
+import java.util.Scanner;
+
 public class C173 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		int m = 3;
+		int n =2;
+		int x[] = new int [m];
+		int y[] = new int [n];
+		int a;
+		int i;
+		int z[] = new int[m+n];
+		Scanner sin = new Scanner(System.in);
+		System.out.println("x[]の要素入力");
+		for(i=0;i<m;i++){
+		a = sin.nextInt();
+		x[i] = a;
+		}
+		System.out.println("y[]の要素入力");
+		for(i=0;i<n;i++){
+		a = sin.nextInt();
+		y[i] = a;
+		}
+		sin.close();
+		for (i = 0; i < m; i++) {
+			z[i] = x[i];
+		}
+		for (i = 0; i < n; i++) {
+			z[i + m] = y[i];
+		}
+		System.out.print("z[] = {");
+		for (i = 0; i < z.length; i++) {
+		if(i == z.length-1)
+			System.out.print(z[i]);
+		else
+			System.out.print(z[i] + ",");
+		}
+		System.out.print("}");
 	}
-
 }
