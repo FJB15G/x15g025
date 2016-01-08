3fcbb1599f79e0b2d8ef15ef848ac2d3c5a7e1ff
unknown <x15g025@PC504N027.chiba-fjb.ac.jp>
2016-01-08 14:42:43 +0900

    no message

---
+++ src/jp/ac/chibafjb/x15g025/kd232/C232.java
@@ -0,0 +1,66 @@
+package jp.ac.chibafjb.x15g025.kd232;
+
+import java.util.Scanner;
+
+public class C232 {
+
+	public static void main(String[] args) {
+		int data[] = {90,80,50,35,15};
+		Scanner sin = new Scanner(System.in);
+		System.out.println("探索値を入力してください。");
+		int s_int = sin.nextInt();
+		int a;
+		a = ArrayProc.binarySearch2(data,s_int);
+		if(a==-1){
+		System.out.println("探索値"+s_int+"は配列Data[]に存在しません。");
+		}
+		else{
+		System.out.println("探索値"+s_int+"は配列Data["+a+"]に存在します。");
+		}
+	}
+
+}
+class ArrayProc{
+	public static int binarySearch1(int[] data,int s){
+		int l = 0;
+		int h = 4;
+		int m =(l+h)/2;
+		for(l=l;l<=h&data[m]!=s;l=l){
+			if(data[m] < s){
+				l = l + 1;
+			}
+			else{
+				h = h - 1;
+			}
+			m = (l + h) / 2;
+		}
+		if(l <= h){
+		return m;
+		}
+		else{
+			return (-1);
+		}
+		
+		
+	}
+	public static int binarySearch2(int[] data,int s){
+		int l = 0;
+		int h = 4;
+		int m =(l+h)/2;
+		for(l=l;l<=h&data[m]!=s;l=l){
+			if(data[m] < s){
+				h = h - 1;
+			}
+			else{
+				l = l + 1;
+			}
+			m = (l + h) / 2;
+		}
+		if(h <= l){
+		return m;
+		}
+		else{
+			return (-1);
+		}
+}
+}
